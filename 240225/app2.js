096354a51f252d6c2b2cafa99004d46b42374ef0
sofiamcas <119833805+sofiamcas@users.noreply.github.com>
2025-03-24 12:01:20 -0600

    Add files via upload

---
+++ 240225/app2.js
@@ -0,0 +1,12 @@
+let fruta = ["Mango", "Palta", "Manzana", "Plátano", "Fresa"];
+
+// for of
+for(let frutas of fruta){
+    console.log(frutas);
+
+}
+
+// for in
+for(let frutas in fruta){
+    console.log(frutas);
+}
